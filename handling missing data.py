c9454b69065f091f63ef2a27687182a26f0f47f2
orc1234 <98529444+orc1234@users.noreply.github.com>
2022-01-27 18:50:10 +0200

    Add files via upload

---
+++ handling missing data.py
@@ -0,0 +1,55 @@
+import numpy as np
+import pandas as pd
+from bs4 import BeautifulSoup
+import matplotlib as mpl
+from matplotlib import pyplot as plt
+import warnings
+warnings.filterwarnings("ignore")
+import matplotlib_inline
+import time
+time.sleep(3)
+
+
+
+podcasts_df = pd.read_csv('podcasts.csv', header=0, sep=',')
+podcasts_df.info()
+
+
+#Handel missing data.
+podcasts_df2=podcasts_df.dropna(axis=0).copy()
+podcasts_df2.info()
+
+#Remove duplicates or irrelevant observations.
+podcasts_df2.drop_duplicates(subset ="podcast_name", keep = "first" , inplace = True)
+podcasts_df2.info()
+#Fix structual errors.
+podcasts_df2['publish_date']=podcasts_df2.publish_date.str[-4:]
+podcasts_df2['top_rank']=podcasts_df2.top_rank.str.replace('%?', '').astype(float)
+podcasts_df2['episodes']=podcasts_df2.episodes.str.replace('episodes?', '').astype(int)
+
+
+#Filter unwanted outliers.
+podcasts_df2.describe(include='all')
+plt.hist(podcasts_df2.top_rank, bins= 100)
+plt.xlabel("top_rank")
+plt.ylabel("Frequence")
+plt.show()
+print(sum(podcasts_df2['top_rank']>100))
+print(sum(podcasts_df2['top_rank']<0))
+
+plt.hist(podcasts_df2.ListenScore, bins= 100)
+plt.xlabel("ListenScore")
+plt.ylabel("Frequence")
+plt.show()
+print(sum(podcasts_df2['ListenScore']>100))
+print(sum(podcasts_df2['ListenScore']<0))
+
+plt.hist(podcasts_df2.episodes, bins= 200)
+plt.axis([0,3500,0,2000])
+plt.xlabel("episodes")
+plt.ylabel("Frequence")
+plt.show()
+print(sum(podcasts_df2['ListenScore']<=0))
+
+podcasts_df2.to_csv('copy_df_podcasts.csv')
+
